9f35f4400347fa385772df0ed969891d88ab53f4
Koen Klaren <koen.klaren@gmail.com>
2016-05-15 15:19:25 +0200

    Refactor backend

--- src/server/index.php
+++ src/server/index.php
@@ -1,3 +1,11 @@
 <?php
 
-require('client.php');
+define('SERVER_ENV', getenv('SERVER_ENV'));
+
+if (SERVER_ENV == 'production') {
+  define('ASSETS_BASE', '/assets/build');
+} else {
+  define('ASSETS_BASE', 'http://localhost:8080');
+}
+
+require('views/client.phtml');
